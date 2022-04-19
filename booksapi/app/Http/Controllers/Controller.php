c99cca7bd2fae9e8da7df94028442fa9a08f14ee
myze <myze2016@gmail.com>
2022-04-19 18:52:54 +0800

    update 1

--- booksapi/app/Http/Controllers/Controller.php
+++ booksapi/app/Http/Controllers/Controller.php
@@ -1,13 +1,37 @@
 <?php
 
 namespace App\Http\Controllers;
-
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin,x-csrf-token');
+header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT');
+header('Access-Control-Allow-Headers', 'Origin, Accept, Content-Type, X-Requested-With');
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+
+
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function __construct()
+    {
+    }
+
+    public function getUser(Request $request){
+        if ($request->getMethod()=='OPTIONS') { return; };
+        $result = \DB::connection()->select('select * from tbluser');
+        return $result;
+    }
+
+    public function login(Request $request){
+        if ($request->getMethod()=='OPTIONS') { return; };
+        $Username = $request->input('Username');
+        $Password = $request->input('Password');
+        $result = \DB::connection()->select('select * from tbluser where Username=? and Password=?',[$Username,$Password]);
+        return $result;
+    }
 }
--- booksapi/routes/web.php
+++ booksapi/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::any('user','App\Http\Controllers\Controller@getUser');
+Route::any('login','App\Http\Controllers\Controller@login');
